418749f4002e5ad17394f4ac3df668d4a11b6560
Jaakko Lindvall <verkel@iki.fi>
2022-10-02 15:16:30 +0300

    TestSong stubs

---
+++ DanceCommonTests/TestSong.cpp
@@ -0,0 +1,53 @@
+// Disable warnings related to using stl from modules and GTest from headers
+#pragma warning(disable: 5050 4005)
+
+#include "gtest/gtest.h"
+import Chart;
+import ChartInfo;
+import PlayStyle;
+import Difficulty;
+import NoteLength;
+import NotePos;
+import NoteRow;
+import NoSuchChartException;
+import <fstream>;
+import <optional>;
+import <string>;
+import <string_view>;
+
+using namespace DanceCommon;
+
+TEST(Song, Load)
+{
+	FAIL();
+}
+
+TEST(Song, ChartInfos)
+{
+	FAIL();
+}
+
+TEST(Song, GetPosition_VertexDelta)
+{
+	FAIL();
+}
+
+TEST(Song, GetPosition_Silikon)
+{
+	FAIL();
+}
+
+TEST(Song, GetTime_VertexDelta)
+{
+	FAIL();
+}
+
+TEST(Song, GetTime_Silikon)
+{
+	FAIL();
+}
+
+TEST(Song, Save)
+{
+	FAIL();
+}
